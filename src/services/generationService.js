40ebee3ff127b18901aae1f0dbc38aa14400cb8f
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2025-11-06 11:14:15 +0000

    Add initial code structure for GenerationService class

---
+++ src/services/generationService.js
@@ -0,0 +1,13 @@
+class GenerationService {
+    constructor() {
+        // Initialize class properties here
+    }
+
+    generate() {
+        // Method to generate something
+    }
+
+    // Add additional methods as needed
+}
+
+export default GenerationService;
